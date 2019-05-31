70298d65a159150d8bf2410382623b0c16a3fd41
Ratko Amanovic <ratkoamanovic@gmail.com>
2019-05-31 23:05:14 +0200

    Dodat moderator frontend

--- frontend/finalf/application/views/list.php
+++ frontend/finalf/application/views/list.php
@@ -1,81 +1,98 @@
 
 <!---->  <section class="page-section about-heading">
     <div class="container">
-      
-      <div class="about-heading-content">
-        <div class="row ">
-          
-          <div class="col-xl-12 col-lg-12 mx-auto ">
-            <div class="bg-faded rounded p-5">
-             <h2 class="section-heading mb-4">
-                <span class="section-heading-upper"><?php ?></span> <!--  if is set za tip numere-->
-               <!--  <span class="section-heading-lower">O nama</span>-->
-              </h2>
-              <!--<p>Mi smo trojica studenata Elektrotehničkog fakulteta koji ovaj sajt rade kao projekat iz predmeta Principi Softverskog Inženjerstva na trećoj godini studija.</p>
-              <p class="mb-0">D Bit tim čine : Ratko Amanović, David Milićević i Andrija Veljković. :')</p>-->
-                <div class ="row">
-                    <div class="col-xl-9 ">
-                        <table class ="table table-striped table-hover">
-                            <thead class="thead-light">
-                                <tr>
-                                  <th scope="col">Autor</th>
-                                  <th scope="col">Delo</th>
-                                </tr>
-                            </thead>
-                            <?php
-                            if (isset($numere)){
-                            foreach ($numere as $numera) {
-                            echo "<tr><td scope = 'col'>".$numera->autor."</td><td scope = 'col'><a href =".site_url("$controller/pesma")."?id=".$numera->id.">"
-                                     .$numera->delo. "</a></td>";
-                            }
-                            
-                            }
-                            ?>
-                        </table>
-                    </div>
-                    </div>
-                <div class ="row">
-                <div class ="col-xl-7">
-                    <form action ="<?php  echo site_url("$controller/muzika");?>" method = "post">
-                        <button type="submit" class="btn btn-warning" name = "prva"> <!-- mozes preko post metoda proveriti sta je pritisnuto -->
-                            Prva</button>
-                        <button type="submit" class="btn btn-warning" name = "prethodna"
-                                <?php
-                                    if (isset($trenStr)){
-                                        if ($trenStr == 1){
+
+        <div class="about-heading-content">
+            <div class="row ">
+
+                <div class="col-xl-12 col-lg-12 mx-auto ">
+                    <div class="bg-faded rounded p-5">
+                        <h2 class="section-heading mb-4">
+                            <span class="section-heading-upper"><?php ?></span> <!--  if is set za tip numere-->
+                           <!--  <span class="section-heading-lower">O nama</span>-->
+                        </h2>
+                        <!--<p>Mi smo trojica studenata Elektrotehničkog fakulteta koji ovaj sajt rade kao projekat iz predmeta Principi Softverskog Inženjerstva na trećoj godini studija.</p>
+                        <p class="mb-0">D Bit tim čine : Ratko Amanović, David Milićević i Andrija Veljković. :')</p>-->
+                        <div class ="row">
+                            <div class="col-xl-9 ">
+                                <table class ="table table-striped table-hover">
+                                    <thead class="thead-light">
+                                        <tr>
+                                            <th scope = 'col'>#</th>
+                                            <th scope = 'col'>Autor</th>
+                                            <th scope = 'col'>Delo</th>
+                                            <?php
+                                            if (isset($odobravanje)) {
+                                                echo "<th scope = 'col'></th>";
+                                            }
+                                            ?>
+                                        </tr>
+                                    </thead>
+                                    <?php
+                                    if (isset($numere)) {
+                                        $id = 1;
+                                        foreach ($numere as $numera) {
+                                            echo "<tr>"
+                                            . "<td scope = 'col'>" . $id++ . "</td>"
+                                            . "<td scope = 'col'>" . $numera->autor . "</td>"
+                                            . "<td scope = 'col'><a href =" . site_url("$controller/pesma") . "?id=" . $numera->id . ">" . $numera->delo . "</a></td>";
+                                            if (isset($odobravanje)) {
+                                                echo "<td scope = 'col'><a class='btn btn-primary' href=" . site_url("$controller/pesma") . " '>Pregledaj</a></td>";
+                                            }
+                                            echo "</tr>";
+                                        }
+                                    }
+                                    ?>
+                                </table>
+                            </div>
+                        </div>
+                        <div class ="row">
+                            <div class ="col-xl-7">
+                                <form action ="<?php echo site_url("$controller/muzika");
+                                    ?>" method = "post">
+                                    <button type="submit" class="btn btn-warning" name = "prva"> <!-- mozes preko post metoda proveriti sta je pritisnuto -->
+                                        Prva</button>
+                                    <button type="submit" class="btn btn-warning" name = "prethodna"
+                                    <?php
+                                    if (isset($trenStr)) {
+                                        if ($trenStr == 1) {
                                             echo "disabled";
                                         }
                                     }
-                                ?>
-                                >
-                            Prethodna</button>
-                        <button type="submit" class="btn btn-warning" name = "sledeca"
-                                <?php
-                                    if (isset($trenStr) && isset($ukupnoStr)){
-                                        if ($trenStr == $ukupnoStr){
+                                    ?>
+                                            >
+                                        Prethodna</button>
+                                    <button type="submit" class="btn btn-warning" name = "sledeca"
+                                    <?php
+                                    if (isset($trenStr) && isset($ukupnoStr)) {
+                                        if ($trenStr == $ukupnoStr) {
                                             echo "disabled";
                                         }
                                     }
+                                    ?>
+                                            >
+                                        Sledeća</button>  <!-- vidi da li treba skok na stranicu da se uradi-->                
+                                </form>
+                            </div>
+                            <div class ="col-xl-2 text-right">
+                                <?php
+                                if (isset($trenStr)) {
+                                    echo $trenStr;
+                                } else {
+                                    echo "?";
+                                }
+                                echo "/";
+                                if (isset($ukupnoStr)) {
+                                    echo $ukupnoStr;
+                                } else {
+                                    echo "?";
+                                }
                                 ?>
-                                >
-                            Sledeća</button>  <!-- vidi da li treba skok na stranicu da se uradi-->                
-                    </form>
+                            </div>
+                        </div> 
+                    </div>
                 </div>
-                <div class ="col-xl-2 text-right">
-                    <?php
-                                    if (isset($trenStr)){
-                                            echo $trenStr;
-                                    }else {echo "?";}
-                                            echo "/";
-                                    if (isset($ukupnoStr)){
-                                            echo $ukupnoStr;
-                                    }else {echo "?";}        
-                                ?>
-                </div>
-                    </div> 
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </section>
+</section>
---
+++ frontend/finalf/application/views/statistika.php
@@ -0,0 +1,24 @@
+<section class="page-section cta">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-9 mx-auto">
+                <div class="cta-inner text-center rounded">
+                    <h2 class="section-heading mb-5">
+                        <span class="section-heading-lower">Statistika</span> 
+                    </h2>
+                    <table class = "table table-striped mt-5">
+                        <tr>
+                            <td>Korisničko ime:</td>
+                            <td><input type = "text" name = "user"></td>
+
+                        </tr>
+                        <tr>
+                            <td>Lozinka</td>
+                            <td><input type = "password" name = "pass"></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
