7cb6413c1124b3b473cef41580fc94551a43d215
Ratko Amanovic <ratkoamanovic@gmail.com>
2019-06-21 08:34:18 +0200

    Hotfix song text formating

--- Projekat/application/views/pesma.php
+++ Projekat/application/views/pesma.php
@@ -12,9 +12,9 @@
 
                         <div class ="row">
                             <div class ="col-xl-8 text-justify">
-                                <?php
+                                <pre><?php
                                 echo file_get_contents($pesma->putanjaDoAkorda);
-                                ?> 
+                                ?> </pre>
                             </div>
                             <div class ="col-xl-4">
 
